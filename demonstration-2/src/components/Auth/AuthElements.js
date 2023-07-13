9be8e11df8afeef7dfac82187ab78ec32b4af56c
ProfessorHusnain <mh571536@gmail.com>
2023-07-13 22:58:35 +0500

    intial

---
+++ demonstration-2/src/components/Auth/AuthElements.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+export const PageContainer = styled.div`
+display: flex;
+gap: 5px;
+justify-content: space-between;
+`
+export const ImageConatiner=styled.img`
+width: 80%;
+ display: flex;
+ justify-content: center;
+`
+export const Image=styled.img`
+width: 100%;
+height: 100%;
+`
+
+export const FormConatiner=styled.div`
+width: 100%;
+`
---
+++ demonstration-2/src/components/Auth/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { FormConatiner, ImageConatiner, PageContainer, Image } from './AuthElements'
+ 
+
+const Auth = () => {
+  return (
+<PageContainer>
+    <ImageConatiner src="/logo192.png">
+        
+
+    </ImageConatiner>
+    <FormConatiner>
+
+    </FormConatiner>
+</PageContainer>
+    )
+}
+
+export default Auth
